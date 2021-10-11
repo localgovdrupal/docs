e928cc02f05a4dec6c09e985bbf40eca43bdfb63
Maria Young <maria@agile.coop>
2021-10-11 17:10:33 +0100

    Fix error in menu

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -115,7 +115,7 @@ module.exports = {
             '/devs/features/news-technical',
             '/devs/features/directories-technical',
             '/devs/features/geo-technical',
-            '/devs/features/subsites'
+            '/devs/features/subsites',
             '/devs/features/workflows',
           ]
         },
