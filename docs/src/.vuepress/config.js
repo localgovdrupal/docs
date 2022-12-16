510dba5f450da5cc198336f4395660507f716940
Stephen Cox <stephen@agile.coop>
2022-12-16 15:11:40 +0000

    Added GA4 code #148

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -24,11 +24,11 @@ module.exports = {
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
-
     ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
     ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
-
+    ['script', {async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-ETT82J89DV'}],
+    ['script', {}, `"window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-ETT82J89DV');`],
   ],
 
   /**
